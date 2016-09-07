eba2d27ca5d0bc9931196676062e9208cbed4510
Ngone51 <820007565@qq.com>
2016-09-07 14:26:55 +0800

    Update optimization.py

--- chapter5/optimization.py
+++ chapter5/optimization.py
@@ -97,9 +97,9 @@ def hillclimb(domain,costf):
     for j in range(len(domain)):
       # One away in each direction
       if sol[j]>domain[j][0]:
-        neighbors.append(sol[0:j]+[sol[j]+1]+sol[j+1:])
-      if sol[j]<domain[j][1]:
         neighbors.append(sol[0:j]+[sol[j]-1]+sol[j+1:])
+      if sol[j]<domain[j][1]:
+        neighbors.append(sol[0:j]+[sol[j]+1]+sol[j+1:])
 
     # See what the best solution amongst the neighbors is
     current=costf(sol)
